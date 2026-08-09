e6fb91351203ac3bee0c56c80fdadd2a29bb25c2
John Grant <john@johngrant.dev>
2026-08-10 02:41:26 +1000

    add the fedora family module

---
+++ fedora-family/module.sh
@@ -0,0 +1,9 @@
+systemctl mask systemd-remount-fs.service
+
+mv /opt /opt.bak
+mkdir /opt
+
+mv /usr/bin/systemctl /usr/bin/systemctl.bak
+ln -s /usr/bin/true /usr/bin/systemctl
+
+dnf5 install -y dnf5-plugins
