13b74d11cfa2df3e1987e551152e3a981458e871
John Grant <john@johngrant.dev>
2026-08-09 16:39:55 +1000

    seed the standard module collection

---
+++ de/plasma-bootc-updates/module.sh
@@ -0,0 +1,2 @@
+source /ctx/lib/fetch-helpers.sh
+fetch_install_rpm "$ASSET_TECTONIC_BOOTC_UPDATES_URL" "$ASSET_TECTONIC_BOOTC_UPDATES_SHA256"
---
+++ virtualization/libvirt/module.sh
@@ -0,0 +1,2 @@
+source /ctx/lib/wrap-helpers.sh
+wrap_no_hardened_malloc /usr/bin/virt-manager
