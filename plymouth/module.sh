986d58bee4095d7494534551d941ab4da3591469
John Grant <john@johngrant.dev>
2026-08-29 22:59:22 +1000

    set the plymouth theme the way each family names it

--- plymouth/module.sh
+++ plymouth/module.sh
@@ -1,1 +1,13 @@
-plymouth-set-default-theme spinner
+# Fedora and Debian ship plymouth-set-default-theme. Ubuntu does not, and picks
+# the theme through the alternatives system instead, where the spinner theme
+# ships on disk but arrives unregistered.
+theme=spinner
+
+if command -v plymouth-set-default-theme >/dev/null; then
+	plymouth-set-default-theme "$theme"
+else
+	file="/usr/share/plymouth/themes/${theme}/${theme}.plymouth"
+	update-alternatives --install /usr/share/plymouth/themes/default.plymouth \
+		default.plymouth "$file" 200
+	update-alternatives --set default.plymouth "$file"
+fi
